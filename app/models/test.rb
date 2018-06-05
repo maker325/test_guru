614357284061746bc8f00ceae4e8b5a86d1676c2
maker325 <maker325@gmail.com>
2018-06-05 13:45:53 +0300

    first

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_passages
+  has_many :users, through: :tests_passages
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
   def self.show_tests_title(category_title)
     Test.joins('JOIN categories
     ON tests.category_id = categories.id')
