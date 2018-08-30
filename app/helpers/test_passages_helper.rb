f387030422044a8ccf6f12f0261d83b04b9799e2
maker325 <maker325@gmail.com>
2018-08-30 14:52:47 +0300

    lesson9-some correction

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,11 @@
 module TestPassagesHelper
   def test_passage_result(result)
-    content_tag(:span, result.to_s, style: 'color: green; font-size: 20px') << if @test_passage.passed?
-                                                                                 content_tag(:p, ' Test passed!')
-                                                                               else
-                                                                                 content_tag(:p, ' Test Failed!')
-                                                              end
+    if @test_passage.passed?
+      content_tag(:span, result.to_s, style: 'color: green; font-size: 20px') <<
+        content_tag(:p, ' Test passed!', style: 'font-size: 25px')
+    else
+      content_tag(:span, result.to_s, style: 'color: red; font-size: 20px') <<
+        content_tag(:p, ' Test Failed!', style: 'font-size: 25px')
+    end
   end
 end
