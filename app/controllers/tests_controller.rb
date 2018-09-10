08d69da52a3df8c49cc43eed001319475ecfc4b9
maker325 <maker325@gmail.com>
2018-09-10 12:37:29 +0300

    lesson10 - last commit

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,5 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :find_user, only: :start
 
   rescue_from ActiveRecord::RecordNotFound,
               with: :rescue_with_test_not_found
@@ -51,10 +50,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = User.first
   end
 
   def test_params
